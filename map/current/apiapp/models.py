7e7d4811e01db7ec0911d32b3f5acdd01de288fc
12momoko33 <106864178+12momoko33@users.noreply.github.com>
2022-09-08 05:34:12 +0000

    Thu, Sep 8, 2022, 5:34 AM +00:00

--- map/current/apiapp/models.py
+++ map/current/apiapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class CurrentLocation(models.Model):
+    longitude = models.CharField(max_length=128)
+    latitude = models.IntegerField()
