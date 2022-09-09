7e090f0895967de629c9a8df59f71e7554bb6f79
12momoko33 <106864178+12momoko33@users.noreply.github.com>
2022-09-09 06:47:45 +0000

    Fri, Sep 9, 2022, 6:47 AM +00:00

--- map/static/js/geo.js
+++ map/static/js/geo.js
@@ -10,6 +10,7 @@ function successCallback(position){
     // 緯度を取得し画面に表示
     var latitude = position.coords.latitude;
     document.getElementById("latitude").innerHTML = latitude;
+    print(var output  = document.getElementById(“text”).innerText;)
     // 経度を取得し画面に表示
     var longitude = position.coords.longitude;
     document.getElementById("longitude").innerHTML = longitude;
