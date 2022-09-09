84049aea1653e11905ef147c586aa0749f159e31
12momoko33 <106864178+12momoko33@users.noreply.github.com>
2022-09-09 07:01:45 +0000

    現在地取得機能途中です

--- map/current/apiapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import CurrentLocation
-@admin.register(CurrentLocation)
-class CurrentLocationAdmin(admin.ModelAdmin):
-    pass
--- map/current/apiapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class CurrentLocation(models.Model):
-    long = models.DecimalField(max_digits=9, decimal_places=6)
-    lat = models.DecimalField(max_digits=9, decimal_places=6)
