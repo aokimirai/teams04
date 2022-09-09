91affd6b495b250175b7db826780b132272ad2ea
12momoko33 <106864178+12momoko33@users.noreply.github.com>
2022-09-09 06:28:14 +0000

    Fri, Sep 9, 2022, 6:28 AM +00:00

--- map/app.py
+++ map/app.py
@@ -44,11 +44,16 @@ db = SQL("sqlite:///map.db")
 def home():
     return render_template("index.html")
 
-@app.route("/gps", methods=['POST'])
+@app.route("/gps", methods=["GET","POST"])
 def gps():
-    lat = request.form['lat']
-    long = request.form['long']
-    return render_template("gps.html")
+    if request.method == "POST":
+        lat = request.form['lat']
+        long = request.form['long']
+        print("緯度",lat)
+        print("経度",long)
+        return render_template("gps.html")
+    else:
+        return render_template("gps.html")
 
 #ポイントカードの処理
 @app.route("/point", methods=["GET","POST"])
