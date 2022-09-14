426ff9e9b809bf115b95131f19363a7d09954e7c
12momoko33 <106864178+12momoko33@users.noreply.github.com>
2022-09-14 02:13:26 +0000

    Wed, Sep 14, 2022, 2:13 AM +00:00

--- map/static/js/favorite.js
+++ map/static/js/favorite.js
@@ -1,20 +1,19 @@
 $(".addtofavorite").click(function(){
     var favorite_pages_start = JSON.parse(localStorage.getItem('favorite_pages'));
-    var title = $("h1.page-title").text(); // ページのタイトルを取得
-    if (!title.length) { // ↑が無い場合は、<head>のタイトルを取得
-        title = document.title;
-    }
-    var favorite_pages = [{
-        url: CCM_CID, // CCM_CIDは、concrete5 が各ページに付与しているID
-        title: title,
-        datetime: $.now()
+    var name = $("{{ via.name }}").text(); // ページのタイトルを取得
+    var duration = $("{{ via.add_duration }}").text(); // ページのタイトルを取得
+    var distance = $("{{ via.add_distance }}").text(); // ページのタイトルを取得
+    var url = $("{{ url[a.cnt] }}").text(); // ページのタイトルを取得
+
+    var favorite = [{
+        name = name
+        duration = duration
+        distance = distance
+        url = url
     }];
 
-    if(favorite_pages_start.length >= 10) { // 最大10個まで
-        alert("お気に入りの登録数の上限に達しました。");
-        return;
-    }
-    if (favorite_pages_start) {
+
+    if (favorite_start) {
         for(i=0;i<10;i++) {
             if (favorite_pages_start[i] && favorite_pages[0].url !== favorite_pages_start[i].url) {
                 favorite_pages.push(favorite_pages_start[i]);
@@ -24,3 +23,9 @@ $(".addtofavorite").click(function(){
     localStorage.setItem('favorite_pages',JSON.stringify(favorite_pages));
     addFavorite();
 });
+
+function addFavorite() {
+    $(".favoritedmark").removeClass('fade');
+    $(".removefavorite").removeClass('hidden');
+    $(".addtofavorite").addClass('hidden');
+}
