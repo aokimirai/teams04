48a2af7abf79e56f1d71ae7241ab646a54dabb70
12momoko33 <106864178+12momoko33@users.noreply.github.com>
2022-09-17 06:57:56 +0000

    Sat, Sep 17, 2022, 6:57 AM +00:00

--- map/static/js/geo.js
+++ map/static/js/geo.js
@@ -1,5 +1,5 @@
 // ボタンを押した時の処理
-window.onload = function(){
+document.getElementById("btn").onclick = function(){
     // 位置情報を取得する
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 };
